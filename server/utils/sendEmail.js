f540ff08ddc78484b1320b162527dc49f8bb5866
EGhost <Adityachauhan8901@gmail.com>
2024-02-11 04:45:52 +0530

    All Routes Working

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,17 +2,16 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 
 async function sendEmail({ to, subject, resetLink }) {
-    // Create a transporter with SMTP transport
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'aditya09proud@gmail.com', 
-            pass: 'bews oqqz fykz sprq'
+            user: process.env.EMAIL_ADDRESS, 
+            pass: process.env.EMAIL_PASSWORD
         }
     });
     const emailTemplate = fs.readFileSync('server/utils/passwordResetTemplate.html', 'utf-8');
     const emailContent = emailTemplate.replace('%RESET_LINK%', resetLink);
-     // Define the email options
+
     const mailOptions = {
         from: 'EGhost <aditya09proud@gmail.com>', 
         to, // Recipient email address
